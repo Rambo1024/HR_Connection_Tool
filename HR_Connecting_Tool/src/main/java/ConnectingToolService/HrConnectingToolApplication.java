dd5dac9814005a18e2adc7c85b62ad68b1367266
Unknown <stefanleuders@yahoo.de>
2020-05-29 14:59:53 +0200

    Neues Projekt

---
+++ HR_Connecting_Tool/src/main/java/ConnectingToolService/HrConnectingToolApplication.java
@@ -0,0 +1,13 @@
+package ConnectingToolService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConnectingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConnectingToolApplication.class, args);
+	}
+
+}
---
+++ HR_Connecting_Tool/src/test/java/ConnectingToolService/HrConnectingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package ConnectingToolService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConnectingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
